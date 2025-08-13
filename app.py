19279507c76985c7c89027d0f5d4b0692e938cf0
Oliver Widjaja <oliverwidjaja@gmail.com>
2025-08-13 16:41:17 +0800

    bad gateway fix

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import xgboost as xgb
 import pandas as pd
+import os
 
 app = Flask(__name__)
 
@@ -38,4 +39,5 @@ def index():
     return app.send_static_file('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
